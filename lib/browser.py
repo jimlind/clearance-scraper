d3b1b7e8feefa5f796190edc7d677fa127abfe49
Jim Lind <spoon.vw@gmail.com>
2016-11-22 01:37:55 +0000

    Move the proxy definitations to a config. Do a better job of attempting to log info. Try and do all the button pushing and security overrides.

--- lib/browser.py
+++ lib/browser.py
@@ -6,47 +6,16 @@ import random
 
 class Browser:
  
-    # Find All Proxies
-    # Google: "Powered by Glype"
-    
-    proxyList = [
-        # BROKEN
-        #   'http://getanonymous.com/index.php',
-        #   'https://thespacesurf.com/index.php',
-        #   'http://ricardoalcala.com/index.php',
-        #   'http://p.pomstiborius.pl/index.php',
-        #   'http://www.blackhost.xyz/glype/index.php',
-        #   'https://muadness.com/proxy/index.php',
-        #   'http://freebritishproxy.com/index.php',
-        # FAILS
-        #   'http://proxy.rimmer.su/index.php',
-        # THROTTLED 
-        #   'http://zero1.ocaspro.com/index.php',
-        # SLOW
-        #   'http://proxy-bg.com/index.php',
-
-        #'https://rgsurf.com/index.php',
-        #'https://cookieparts.com/click/index.php',
-        #'https://hidethedork.com/index.php',
-        #'http://theproxy.co/index.php', 
-
-	'https://foxhold.com/web/index.php',
-        'http://samstevenm.com/prox/index.php',
-        'http://thehidden.ninja/index.php',
-        'http://proxy.doomoney.com/index.php',
-        'http://mail.germanystudy.net/index.php',
-        'http://bypass.germanystudy.net/index.php',
-        'http://mail.travelpakistan.org/index.php',
-        'http://bypass.travelpakistan.org/index.php',
-        # HALF BROKEN 'http://antoinem.com/preauxy/index.php',
-        'http://blooby.eu/index.php',
-        'http://www.crankedcode.com/projects/glype-youtube/index.php',
-        'https://serviceone.tk/glype/index.php',
-    ]
+    proxyList = []
+    check = ''
     mechBrowser = None
-    courtesyTime = 3
+    courtesyTime = 2
 
-    def __init__(self):
+    def __init__(self, proxyList, check):
+        self.proxyList = proxyList
+        self.check = check
+        
+    def setup(self):
         self.mechBrowser = mechanize.Browser()
         cookieJar = cookielib.LWPCookieJar()
         self.mechBrowser.set_cookiejar(cookieJar)
@@ -57,30 +26,73 @@ class Browser:
         self.mechBrowser.set_handle_robots(False)
         self.mechBrowser.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
-        agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
+        agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:48.0) Gecko/20100101 Firefox/48.0'
         self.mechBrowser.addheaders = [('User-agent', agent)]
+
+    def shutdown(self):
+        self.mechBrowser.close()
 
     def sleep(self):
         time.sleep(self.courtesyTime + random.randint(1, self.courtesyTime))
 
-    def getHtml(self, url):
-        proxyUrl = random.choice(self.proxyList) + '?b=24'
-        # print proxy if trying to debug
-        print(proxyUrl)
-        self.mechBrowser.open(proxyUrl)
-        self.sleep()
-
-        self.mechBrowser.select_form(nr=0)
-        self.mechBrowser.form['u'] = url
-        self.mechBrowser.submit()
-        self.sleep()
-
-        return self.mechBrowser.response().read()
-
     def getSource(self, url):
-        try:
-            return self.getHtml(url)
+        while (True):
+            self.sleep()
+            source = self.getSourceOrFalse(url)
+            if (False != source):
+                return source
+
+    def getSourceOrFalse(self, url):
+        proxyUrl = random.choice(self.proxyList) + '?b=24'
+        print('Proxy URL ' + proxyUrl)
+        print('Store URL ' + url)
+
+	try:
+            self.mechBrowser.open(proxyUrl)
+
+            formCount = 0
+            for form in self.mechBrowser.forms():
+                if (str(form.attrs['action']).find('process.php?') > 0):
+                    break
+                formCount = formCount+1
+            self.mechBrowser.select_form(nr=formCount)
+        
+            self.mechBrowser.form['u'] = url
+            self.mechBrowser.submit()
         except:
-            # print failure message if trying to debug
-            print('failed')
-            return self.getSource(url)
+            return self.fail(proxyUrl, 'Form Submission Failure')
+ 
+        try:
+            source = self.mechBrowser.response().read()
+        except:
+            return self.fail(proxyUrl, 'Response Read Failure')
+
+        # Hack to compensate for XHTML etc
+        self.mechBrowser._factory.is_html = True
+
+        if 'Security Warning' == self.mechBrowser.title():
+            print('**** Security Override')
+            try:
+                self.mechBrowser.select_form(nr=0)
+                self.mechBrowser.submit()
+                response = self.mechBrowser.response()
+                source = response.get_data()
+            except:
+                return self.fail(proxyUrl, 'Security Warning Override Failure')
+
+        if (source.find(self.check) == -1):
+            f = open("source.txt","w")
+            f.write(source)
+            f.close()
+            print(self.check)
+            return self.fail(proxyUrl, 'Source Verficiation Failure')
+
+        return source
+
+    # TODO: Actually Log These Failures
+    def fail(self, proxyUrl, message):
+        print('Log Failure!')
+        print(proxyUrl)
+        print(message)
+
+        return False
