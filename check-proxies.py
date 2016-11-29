0776bb86708fb1846125818603e6c8d88dace2e1
Jim Lind <spoon.vw@gmail.com>
2016-11-29 02:55:24 +0000

    Make all the things pretty.

--- check-proxies.py
+++ check-proxies.py
@@ -1,18 +1,17 @@
 from datetime import datetime
 from ConfigParser import ConfigParser
 from lib.browser import Browser
+from colorama import Fore, Back, Style
 
 config = ConfigParser()
 config.read('settings.cfg')
 
 check = config.get('site', 'check')
 url = config.get('site', 'start')
-print('Store URL === ' + url)
 
 rawProxyList = config.get('proxy', 'list').split()
 for rawProxy in rawProxyList:
     proxyUrl = rawProxy.strip()
-    print('Proxy URL === ' + proxyUrl)
     browser = Browser([proxyUrl], check)
     browser.setup()
     startTime = datetime.now()
@@ -21,9 +20,11 @@ for rawProxy in rawProxyList:
     browser.shutdown()
 
     if (False == source):
-        print('          >>> FAILURE! Unable to use this proxy at this time.')
+        print(Fore.RED + 'FAILURE!' + Style.RESET_ALL +  ' | ' + proxyUrl)
         continue;
 
     totalTime = endTime - startTime
-    totalSecondsString = str(totalTime.total_seconds())
-    print('          >>> SUCCESS! It took ' + totalSecondsString + ' seconds on this proxy.')
+    message = Fore.GREEN + 'SUCCESS!' + Style.RESET_ALL + ' | '
+    message += Fore.WHITE + Back.BLUE + (' ' + str(totalTime.total_seconds()) + 's ').ljust(12) + Style.RESET_ALL + ' | '
+    message += proxyUrl
+    print(message)
