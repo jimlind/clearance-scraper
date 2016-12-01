a400a78d6d76204b0ee42ec2517bee6d737965ea
Jim Lind <spoon.vw@gmail.com>
2016-12-01 03:25:33 +0000

    Don't log info for normal scraping

--- scrape.py
+++ scrape.py
@@ -5,7 +5,7 @@ from lib.product_builder import ProductBuilder
 from lib.database import Database
 from telegram import Bot
 
-logging.basicConfig(filename='browser.log', level=logging.DEBUG)
+logging.basicConfig(filename='browser.log', level=logging.WARNING)
 logging.getLogger('Browser')
 
 config = ConfigParser()
