579896a3a0a6251e72acd671c2e164022920c847
Jim Lind <spoon.vw@gmail.com>
2017-01-13 21:57:23 -0500

    Overhaul of the scrape base script.

--- scrape.py
+++ scrape.py
@@ -1,11 +1,12 @@
+#!/usr/bin/python
 import util.logger
 
 from ConfigParser import ConfigParser
 from lib.browser import Browser
-from lib.category_builder import CategoryBuilder
-from lib.product_builder import ProductBuilder
+from lib.category_page import CategoryPage
 from lib.database import Database
 from telegram import Bot
+
 
 # Setup debug logging
 util.logger.setupWarning();
@@ -13,31 +14,44 @@ util.logger.setupWarning();
 config = ConfigParser()
 config.read('settings.cfg')
 
+browser = Browser(config.get('site', 'check'))
 database = Database(config.get('db', 'path') + config.get('db', 'file'))
 database.updateTime()
 
-categoryBuilder = CategoryBuilder(database)
 telegramBot = Bot(token = config.get('bot', 'token'))
 chatId = config.get('bot', 'chat')
-
-rawProxyList = config.get('proxy', 'list').split()
-proxyList = []
-for rawProxy in rawProxyList:
-    proxyList.append(rawProxy.strip())
-check = config.get('site', 'check')
-browser = Browser(proxyList, check)
+url = config.get('site', 'start')
 
 nextCategoryAvailable = True
-url = config.get('site', 'start')
 while nextCategoryAvailable:
     browser.setup()
     source = browser.getSource(url)
     browser.shutdown()
 
-    nextCategoryAvailable = categoryBuilder.parse(source)
-    url = categoryBuilder.getNextUrl()
+    categoryPage = CategoryPage(source)
+    for product in categoryPage.getProductList():
+        previouslyExisted = database.skuExists(product.getProductSku())
+        database.upsertItem(product.getProductSku(), product.getProductUrl())
+        if (previouslyExisted):
+            continue
 
-database.cleanOldItems()
+        if (product.getProductImageUrl().find('data:image') != 0):
+            telegramBot.sendPhoto(chat_id = chatId, photo = product.getProductImageUrl())
+
+        message = product.getProductUrl() + '\n\n'
+        message += product.getProductName() + '\n'
+        message += product.getProductPrice() + '\n'
+        for feature in product.getProdutFeatureList():
+            message += u' \u2022 ' + feature + '\n'
+
+        message += product.getProductStars() + u' \u2606 ' + product.getProductReviews()
+        telegramBot.sendMessage(chat_id=chatId, text=message)
+
+    url = categoryPage.getNextUrl()
+    if ('' == url):
+        nextCategoryAvailable = False
+
+# database.cleanOldItems()
 reportData = database.report()
 
 message = 'Scrape Complete!\n'
@@ -45,22 +59,3 @@ message += str(reportData[0]) + ' New Products Found\n'
 message += str(reportData[1]) + ' Products Avaiable\n'
 message += str(reportData[2]) + ' Products Eliglble for Delete'
 telegramBot.sendMessage(chat_id=chatId, text=message)
-
-productBuilder = ProductBuilder()
-for data in database.getNewUrlList():
-    url = data[0]
-
-    browser.setup()
-    source = browser.getSource(url)
-    browser.shutdown()
-
-    product = productBuilder.parse(source)
-    if ('href' in product and product['href']):
-        telegramBot.sendPhoto(chat_id=chatId, photo=product['href'])
-
-    message = url + '\n\n'
-    for item in product['list']:
-        message += item['p'] + ' :: ' + item['d'] + '\n'
-
-    if message:
-        telegramBot.sendMessage(chat_id=chatId, text=message)
